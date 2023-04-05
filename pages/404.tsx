c0cf5d17af4f4e0f74fa2a7bbad636203ebc7057
hasibmiraz <hasibmiraz09@gmail.com>
2023-04-05 15:26:20 +0600

    Setup complete

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,5 @@
-import styles from '../styles/NotFound.module.css';
-
 const NotFound = () => {
-  return <h1 className={styles.container}>Not found</h1>;
+  return <h1>Not found</h1>;
 };
 
 export default NotFound;
