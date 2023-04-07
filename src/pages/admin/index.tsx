98a1ae3fe7633dba73110b9c3170dba63174b5de
hasibmiraz <hasibmiraz09@gmail.com>
2023-04-08 03:09:58 +0600

    Navbar and add post button complete

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,26 +1,13 @@
 import { NextPage } from 'next';
-import AdminNav from '../../../components/common/AdminNav';
-import {
-  AiOutlineDashboard,
-  AiOutlineContainer,
-  AiOutlineTeam,
-  AiOutlineMail,
-} from 'react-icons/ai';
+import AdminLayout from '../../../components/layout/AdminLayout';
 
 interface Props {}
 
-const navItems = [
-  { href: '/admin', icon: AiOutlineDashboard, label: 'Dashboard' },
-  { href: '/admin/posts', icon: AiOutlineContainer, label: 'Posts' },
-  { href: '/admin/users', icon: AiOutlineTeam, label: 'Users' },
-  { href: '/admin/comments', icon: AiOutlineMail, label: 'Comments' },
-];
-
 const Admin: NextPage<Props> = () => {
   return (
-    <div>
-      <AdminNav navItems={navItems} />
-    </div>
+    <AdminLayout>
+      <div>lorem2000</div>
+    </AdminLayout>
   );
 };
 
