dbcbaf6ce32b98266d4ae750b33c7d9f3b540392
hasibmiraz <hasibmiraz09@gmail.com>
2023-04-08 02:55:55 +0600

    Close toggle design complete

--- components/common/AdminNav.tsx
+++ components/common/AdminNav.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { FC } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
 import Logo from './Logo';
 import { RiMenuFoldFill, RiMenuUnfoldFill } from 'react-icons/ri';
 import { IconType } from 'react-icons/lib';
@@ -12,15 +12,60 @@ interface Props {
   }[];
 }
 
+const NAV_OPEN_WIDTH = 'w-60';
+const NAV_CLOSE_WIDTH = 'w-12';
+const NAV_VISIBILITY = 'nav-visibility';
+
 const AdminNav: FC<Props> = ({ navItems }): JSX.Element => {
+  const navRef = useRef<HTMLElement>(null);
+  const [visible, setVisible] = useState(true);
+
+  const toggleNav = (visibility: boolean) => {
+    const { current: currentNav } = navRef;
+    if (!currentNav) return;
+    const { classList } = currentNav;
+    if (visibility) {
+      // hide nav
+      classList.remove(NAV_OPEN_WIDTH);
+      classList.add(NAV_CLOSE_WIDTH);
+    } else {
+      // show nav
+      classList.remove(NAV_CLOSE_WIDTH);
+      classList.add(NAV_OPEN_WIDTH);
+    }
+  };
+
+  const updateNavState = () => {
+    toggleNav(visible);
+    const newState = !visible;
+    setVisible(newState);
+    localStorage.setItem(NAV_VISIBILITY, JSON.stringify(!visible));
+  };
+
+  useEffect(() => {
+    const navState = localStorage.getItem(NAV_VISIBILITY);
+    if (navState !== null) {
+      const newState = JSON.parse(navState);
+      setVisible(newState);
+      toggleNav(!newState);
+    } else {
+      setVisible(true);
+    }
+  }, []);
+
   return (
-    <nav className="h-screen w-60 shadow-sm bg-secondary-light dark:bg-secondary-dark flex flex-col justify-between">
+    <nav
+      ref={navRef}
+      className="h-screen w-60 shadow-sm bg-secondary-light dark:bg-secondary-dark flex flex-col justify-between transition-width overflow-hidden"
+    >
       <div>
         <Link href="/admin" className="flex items-center space-x-2 p-3 mb-10">
           <Logo className="fill-highlight-light dark:fill-highlight-dark w-5 h-5" />
-          <span className="text-highlight-light dark:text-highlight-dark text-xl font-semibold">
-            Admin
-          </span>
+          {visible && (
+            <span className="text-highlight-light dark:text-highlight-dark text-xl font-semibold leading-none">
+              Admin
+            </span>
+          )}
         </Link>
         {/* Nav items */}
         <div className="space-y-6">
@@ -31,14 +76,23 @@ const AdminNav: FC<Props> = ({ navItems }): JSX.Element => {
               className="flex items-center p-3 text-highlight-light dark:text-highlight-dark text-xl hover:scale-[0.98] transition"
             >
               <item.icon size={24} />
-              <span className="ml-2">{item.label}</span>
+              {visible && (
+                <span className="ml-2 leading-none">{item.label}</span>
+              )}
             </Link>
           ))}
         </div>
       </div>
       {/* Nav toggler */}
-      <button className="text-highlight-light dark:text-highlight-dark p-3 hover:scale-[0.98] transition self-end">
-        <RiMenuFoldFill size={24} />
+      <button
+        onClick={updateNavState}
+        className="text-highlight-light dark:text-highlight-dark p-3 hover:scale-[0.98] transition self-end"
+      >
+        {visible ? (
+          <RiMenuFoldFill size={24} />
+        ) : (
+          <RiMenuUnfoldFill size={24} />
+        )}
       </button>
     </nav>
   );
