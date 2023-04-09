dd4d3a3d685ea66c76f15c2d7cfd18685a55d759
hasibmiraz <hasibmiraz09@gmail.com>
2023-04-10 03:39:28 +0600

    Select the whole link in one click

--- components/editor/index.tsx
+++ components/editor/index.tsx
@@ -1,5 +1,5 @@
-import { FC } from 'react';
-import { useEditor, EditorContent } from '@tiptap/react';
+import { FC, useState, useEffect } from 'react';
+import { useEditor, EditorContent, getMarkRange, Range } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 import Underline from '@tiptap/extension-underline';
 import Placeholder from '@tiptap/extension-placeholder';
@@ -9,6 +9,8 @@ import ToolBar from './ToolBar';
 interface Props {}
 
 const Editor: FC<Props> = (props): JSX.Element => {
+  const [selectionRange, setSelectionRange] = useState<Range>();
+
   const editor = useEditor({
     extensions: [
       StarterKit,
@@ -26,12 +28,27 @@ const Editor: FC<Props> = (props): JSX.Element => {
       }),
     ],
     editorProps: {
+      handleClick(view, pos, event) {
+        const { state } = view;
+        const selectionRange = getMarkRange(
+          state.doc.resolve(pos),
+          state.schema.marks.link
+        );
+        if (selectionRange) setSelectionRange(selectionRange);
+      },
       attributes: {
         class:
           'prose prose-lg focus:outline-none dark:prose-invert max-w-full mx-auto h-full',
       },
     },
   });
+
+  useEffect(() => {
+    if (editor && selectionRange) {
+      editor.commands.setTextSelection(selectionRange);
+    }
+  }, [editor, selectionRange]);
+
   return (
     <div className="p-3 dark:bg-primary-dark bg-primary transition max-w-4xl mx-auto">
       <ToolBar editor={editor} />
